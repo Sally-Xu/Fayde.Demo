a71b8d897997cac833929c8abfe008c5fa9efc03
sladapter <sally_xu@hotmail.com>
2014-02-17 14:28:14 -0500

    rename DafaultViewModel.ts to DefaultViewModel.ts

---
+++ Controls.Demo/ViewModels/DefaultViewModel.js
@@ -0,0 +1,60 @@
+/// <reference path="../lib/Fayde/Fayde.d.ts" />
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+define(["require", "exports", "Models/TreeData"], function(require, exports, MenuItem) {
+    var DefaultViewModel = (function (_super) {
+        __extends(DefaultViewModel, _super);
+        function DefaultViewModel() {
+            _super.call(this);
+            this.Menu = new Fayde.Collections.ObservableCollection();
+            this.Load();
+        }
+        DefaultViewModel.prototype.Load = function () {
+            var item = new MenuItem("Controls");
+            this.Menu.Add(item);
+            item.Children.Add(new MenuItem("Input Controls", "Input"));
+
+            item.Children.Add(new MenuItem("ItemsControl", "ItemsControl"));
+            item.Children.Add(new MenuItem("DatePicker", "DatePicker"));
+            item.Children.Add(new MenuItem("Calendar", "Calendar"));
+            item.Children.Add(new MenuItem("GridSpliter", "GridSpliter"));
+            item.Children.Add(new MenuItem("TreeView", "TreeView"));
+            item.Children.Add(new MenuItem("Tab Control", "Tab"));
+            item.Children.Add(new MenuItem("Accordion", "Accordion"));
+
+            var item = new MenuItem("Layout Panels");
+            this.Menu.Add(item);
+            item.Children.Add(new MenuItem("Grid", "TreeView"));
+            item.Children.Add(new MenuItem("StackPanel", "Home"));
+            item.Children.Add(new MenuItem("DockPanel", "TreeView"));
+            item.Children.Add(new MenuItem("WrapPanel", "Home"));
+
+            var item = new MenuItem("DataVisualization");
+            this.Menu.Add(item);
+            item.Children.Add(new MenuItem("Area Series", "TreeView"));
+            item.Children.Add(new MenuItem("Bar Series", "TreeView"));
+            item.Children.Add(new MenuItem("Bubble Series", "TreeView"));
+            item.Children.Add(new MenuItem("Column Series", "TreeView"));
+            item.Children.Add(new MenuItem("Line Series", "TreeView"));
+            item.Children.Add(new MenuItem("Pie Series", "TreeView"));
+            item.Children.Add(new MenuItem("Scatter Series", "TreeView"));
+            item.Children.Add(new MenuItem("Stacked Series", "TreeView"));
+            item.Children.Add(new MenuItem("TreeMap", "TreeView"));
+
+            var item = new MenuItem("Navigation", "Navigation");
+            this.Menu.Add(item);
+
+            var item = new MenuItem("Theming", "Theming");
+            this.Menu.Add(item);
+        };
+        return DefaultViewModel;
+    })(Fayde.MVVM.ViewModelBase);
+
+    
+    return DefaultViewModel;
+});
+//# sourceMappingURL=DefaultViewModel.js.map
---
+++ Controls.Demo/ViewModels/TabViewModel.js
@@ -0,0 +1,54 @@
+/// <reference path="../lib/Fayde.Controls/Fayde.Controls.Ex.d.ts" />
+/// <reference path="../lib/Fayde/Fayde.d.ts" />
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+define(["require", "exports"], function(require, exports) {
+    var TabViewModel = (function (_super) {
+        __extends(TabViewModel, _super);
+        function TabViewModel() {
+            _super.call(this);
+            this.Tabs = new Fayde.Collections.ObservableCollection();
+            this._addTabCommand = null;
+            this.Load();
+        }
+        Object.defineProperty(TabViewModel.prototype, "AddTabCommand", {
+            get: function () {
+                var _this = this;
+                if (this._addTabCommand === null) {
+                    this._addTabCommand = new Fayde.MVVM.RelayCommand(function () {
+                        return _this.AddTab();
+                    }, function () {
+                        return _this.CanAddTab();
+                    });
+                }
+                return this._addTabCommand;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        TabViewModel.prototype.AddTab = function () {
+            var index = this.Tabs.Count + 1;
+            var item = new Fayde.Controls.TabItem();
+            item.Header = "Tab" + index;
+            item.Content = "Content in Tab" + index;
+            this.Tabs.Add(item);
+        };
+        TabViewModel.prototype.CanAddTab = function () {
+            return this.Tabs.Count < 10;
+        };
+
+        TabViewModel.prototype.Load = function () {
+            this.AddTab();
+            this.AddTab();
+        };
+        return TabViewModel;
+    })(Fayde.MVVM.ViewModelBase);
+    
+    return TabViewModel;
+});
+//# sourceMappingURL=TabViewModel.js.map
